f9640fc91d3886d44a730b0c4c8eadbe3a636dc3
NatalyaKuleshova <nat.kule@yandex.com>
2023-04-14 15:52:42 +0400

    Добавила всё решение

---
+++ Practice/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Task1();
+            //Задание1
+            //Задание2
+            //Задание3
+        }
+        internal static void Task1()
+        {
+            Console.Write("Введите длинну массива: ");
+            int length = int.Parse(Console.ReadLine());
+
+            int[] array = new int[length];
+
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("Введите элемент под индексом {0}: ", i);
+                string input = Console.ReadLine();
+                array[i] = int.Parse(input);
+                sum += array[i];
+            }
+
+            //Console.ReadKey();
+
+            //int sum = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    sum += array[i];  
+            //}
+
+            Console.WriteLine("Сумма числе в массиве равна: " + sum);
+            Console.ReadKey();
+
+        }
+
+    }
+}
+
